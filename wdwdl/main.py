33c30e47866b1d8e30028ee43129f7217969a332
SvWe <weinzierl.sven@web.de>
2019-11-10 20:55:09 +0100

    Removed cross validation from main script.

--- wdwdl/main.py
+++ wdwdl/main.py
@@ -29,27 +29,3 @@ if __name__ == '__main__':
     utils.clear_measurement_file(args)
 
 
-
-    """
-    if args.cross_validation:
-
-        for iteration_cross_validation in range(0, args.num_folds):
-            preprocessor.data_structure['support']['iteration_cross_validation'] = iteration_cross_validation
-            output["training_time_seconds"].append(train_dnn.train(args, preprocessor))
-            test_dnn.test(args, preprocessor)
-            output = utils.get_output(args, preprocessor, output)
-            utils.print_output(args, output, iteration_cross_validation)
-            utils.write_output(args, output, iteration_cross_validation)
-
-        utils.print_output(args, output, iteration_cross_validation + 1)
-        utils.write_output(args, output, iteration_cross_validation + 1)
-
-    # split validation
-    else:
-        train_dnn(args, preprocessor)
-        output["training_time_seconds"].append(train_dnn.train(args, preprocessor))
-        test_dnn.test(args, preprocessor)
-        output = utils.get_output(args, preprocessor, output)
-        utils.print_output(args, output, -1)
-        utils.write_output(args, output, -1)
-    """
