87b84c599ab1ea3a9ddef5cba4da870ad58fb976
SvWe <weinzierl.sven@web.de>
2019-11-15 17:59:35 +0100

    Integrated saving and loading logic for one hot encoders created for each categorical attribute.

--- wdwdl/trainer.py
+++ wdwdl/trainer.py
@@ -5,23 +5,35 @@ import keras
 def train_nn_wa_classification(args, data_set, label):
     """ We use an deep artificial neural network for learning the mapping of process instances and the label. """
 
+    """
     input_layer = keras.layers.Input(shape=(data_set.shape[1], ), name='input_layer')
-    layer_1 = keras.layers.Dense(100, activation='tanh')(input_layer)
+    layer_1 = keras.layers.Conv1D(filters=256, kernel_size=3, padding='valid', activation='relu', strides='1')(input_layer)
+    layer_1 = keras.layers.GlobalAveragePooling1D()(layer_1)
+    layer_1 = keras.layers.Dense(250, activation='relu')(layer_1)
+    b1 =  keras.layers.Dropout(0.2)(layer_1)
+    """
+
+
+    layer_1 = keras.layers.Dense(200, activation='tanh')(input_layer)
     layer_2 = keras.layers.Dropout(0.2)(layer_1)
-    layer_2 = keras.layers.Dense(100, activation='tanh')(layer_2)
+    layer_2 = keras.layers.Dense(150, activation='tanh')(layer_2)
     layer_3 = keras.layers.Dropout(0.2)(layer_2)
     layer_3 = keras.layers.Dense(100, activation='tanh')(layer_3)
-    b1 = keras.layers.Dropout(0.2)(layer_3)
+    layer_4 = keras.layers.Dropout(0.2)(layer_3)
+    layer_4 = keras.layers.Dense(50, activation='tanh')(layer_4)
+    b1 = keras.layers.Dropout(0.2)(layer_4)
+    
 
     output = keras.layers.core.Dense(label.shape[1], activation='softmax', name='output',
                                            kernel_initializer='glorot_uniform')(b1)
 
     model = keras.models.Model(inputs=[input_layer], outputs=[output])
-    #optimizer = keras.optimizers.Adadelta(lr=1.0, rho=0.95)
-    #optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+
     optimizer = keras.optimizers.Nadam(lr=args.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-8, schedule_decay=0.004, clipvalue=3)
+    # optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+
     model.compile(loss={'output': 'categorical_crossentropy'}, optimizer=optimizer)
-    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
     model_checkpoint = keras.callbacks.ModelCheckpoint('%sclf_wa_mapping.h5' % args.checkpoint_dir,
                                                        monitor='val_loss',
                                                        verbose=0,
