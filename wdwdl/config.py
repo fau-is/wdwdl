c51ca795bcf27020c8edabafc469a2aaef736495
sven.weinzierl <weinzierl.sven@web.de>
2019-12-23 19:33:37 +0100

    Cleaning of config.

--- wdwdl/config.py
+++ wdwdl/config.py
@@ -7,28 +7,28 @@ def load():
     parser = argparse.ArgumentParser()
 
     # dnn
-    parser.add_argument('--dnn_num_epochs', default=1, type=int)
-    parser.add_argument('--dnn_num_epochs_auto_encoder', default=1, type=int)
-    parser.add_argument('--dnn_architecture', default=0, type=int)
+    parser.add_argument('--dnn_num_epochs', default=100, type=int)
+    parser.add_argument('--dnn_num_epochs_auto_encoder', default=100, type=int)
+    parser.add_argument('--learning_rate', default=0.002, type=float)
 
-    # pre-processing (min_max_norm, bin, onehot)
+    # pre-processing: min_max_norm, bin or onehot
     parser.add_argument('--encoding_num', default="min_max_norm", type=str)  # for numerical attributes
-    parser.add_argument('--encoding_cat', default="onehot", type=str)  # for categorical attributes
-    parser.add_argument('--num_hash_output', default=2, type=int)  # number of output columns of hash encoding
+    # note: onehot encoding of the categorical attributes can lead to memory errors
+    parser.add_argument('--encoding_cat', default="bin", type=str)  # for categorical attributes
 
     # all models
     parser.add_argument('--task', default="workaround_detection")
-    parser.add_argument('--learning_rate', default=0.002, type=float)  # dnc 0.0001 #dnn 0.002
-
-    # evaluation
-    parser.add_argument('--num_folds', default=3, type=int)  # 10
-    parser.add_argument('--cross_validation', default=False, type=utils.str2bool)
-    parser.add_argument('--split_rate_test', default=0.7, type=float)  # only if cross validation is deactivated
     parser.add_argument('--batch_size_train', default=256, type=int)
     parser.add_argument('--batch_size_test', default=1, type=int)
 
+    # evaluation
+    # note: cross validation is not implemented; only split validation
+    parser.add_argument('--num_folds', default=3, type=int)  # 10
+    parser.add_argument('--cross_validation', default=False, type=utils.str2bool)
+    parser.add_argument('--split_rate_test', default=0.7, type=float)
+
     # data
-    parser.add_argument('--data_set', default="bpi2019_converted_selection.csv")  #bpi2012w_converted_sample.csv")
+    parser.add_argument('--data_set', default="bpi2019_converted_selection.csv")
     parser.add_argument('--data_dir', default="./data/")
     parser.add_argument('--checkpoint_dir', default="./checkpoints/")
     parser.add_argument('--result_dir', default="./results/")
