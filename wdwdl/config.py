d664cc5e2a188c89898138f40aaf9de98ec4e4c7
svwe <weinzierl.sven@web.de>
2020-08-06 14:18:58 +0200

    Removed tools.

--- wdwdl/config.py
+++ wdwdl/config.py
@@ -6,28 +6,29 @@ import wdwdl.utils as utils
 def load():
     parser = argparse.ArgumentParser()
 
-    # dnn
-    parser.add_argument('--dnn_num_epochs', default=100, type=int)
-    parser.add_argument('--dnn_num_epochs_auto_encoder', default=100, type=int)
-    parser.add_argument('--learning_rate', default=0.002, type=float)
+    # Data and pre-processing
+    parser.add_argument('--data_set', default="bpi2013i_converted_selection.csv")
+    parser.add_argument('--data_dir', default="./data/")
+    parser.add_argument('--checkpoint_dir', default="./checkpoints/")
+    parser.add_argument('--result_dir', default="./results/")
+    parser.add_argument('--encoding_num', default="min_max_norm", type=str)  # for numerical attributes min-max norm
+    parser.add_argument('--encoding_cat', default="bin", type=str)  # for categorical attributes binary encoding;
+    # note one-hot encoding of the categorical attributes can lead to memory errors
 
-    # pre-processing: min_max_norm, bin or one-hot
-    parser.add_argument('--encoding_num', default="min_max_norm", type=str)  # for numerical attributes
-    # note: one-hot encoding of the categorical attributes can lead to memory errors
-    parser.add_argument('--encoding_cat', default="bin", type=str)  # for categorical attributes
-
-    # all models
+    # Training
     parser.add_argument('--task', default="workaround_detection")
-    parser.add_argument('--batch_size_train', default=256, type=int)
+    parser.add_argument('--dnn_num_epochs', default=1, type=int)
+    parser.add_argument('--dnn_num_epochs_auto_encoder', default=1, type=int)
+    parser.add_argument('--batch_size_train', default=128, type=int)
     parser.add_argument('--batch_size_test', default=1, type=int)
 
-    # evaluation
-    # note: cross validation is not implemented; only split validation
+    # Evaluation
+    # Note cross validation is not implemented; only split validation
     parser.add_argument('--num_folds', default=3, type=int)  # 10
     parser.add_argument('--cross_validation', default=False, type=utils.str2bool)
     parser.add_argument('--split_rate_test', default=0.7, type=float)
 
-    # hyperparameter optimization
+    # Hyper-parameter optimization
     parser.add_argument('--hpopt', default=True, type=utils.str2bool)
     parser.add_argument('--hpopt_eval_runs', default=5, type=int)
     parser.add_argument('--split_rate_test_hpopt', default=0.2, type=float)
@@ -39,13 +40,8 @@ def load():
     parser.add_argument('--hpopt_kernel_initializer', default=['random_normal', 'random_uniform', 'glorot_normal',
                                                                'glorot_uniform', 'truncated_normal', 'zeros'], type=list)
 
-    # data
-    parser.add_argument('--data_set', default="bpi2013i_converted_selection.csv")
-    parser.add_argument('--data_dir', default="./data/")
-    parser.add_argument('--checkpoint_dir', default="./checkpoints/")
-    parser.add_argument('--result_dir', default="./results/")
 
-    # gpu processing
+    # Gpu processing
     parser.add_argument('--gpu_ratio', default=1.0, type=float)
     parser.add_argument('--cpu_num', default=6, type=int)
     parser.add_argument('--gpu_device', default="0", type=str)
