8d4a330c0ce62d733e50b607288f720ac25a2fcf
sven.weinzierl <weinzierl.sven@web.de>
2020-08-08 01:15:14 +0200

    Update of setup.

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='canepwdl',
-    version='0.1.0',
-    description='Next event prediction with deep learning',
+    name='wdwdl',
+    version='1.0.0',
+    description='Workaround detection with deep learning in event log data',
     long_description=readme,
     author='Sven Weinzierl',
     author_email='sven.weinzierl@fau.de',
-    url='https://github.com/fau-is/next-event-predicton-with-deep-learning',
+    url='https://github.com/fau-is/wdwdl',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
